0a80b5814edfda5551cf7a6a99e3d5fe3a5fd890
sanarjo <sabariask1307@gmail.com>
2020-12-26 20:40:59 +0530

    edit

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'i#kbn&8zdb)ns)b4-@_n&8f5emagu9judy&g@irw2*2qkc22x5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
