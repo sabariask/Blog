e74d021c4938bbf2629fb7c039e18626f2e5a627
sabariask <sabarinathan.mani13@gmail.com>
2023-09-27 11:25:28 +0530

    Added new changes

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import redirect, render,get_object_or_404
 from .models import Post,Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
